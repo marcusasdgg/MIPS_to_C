dc329cef0b53cacbb237dec89c2538db30ac6fc9
marcusasdgg <marcusolachan@icloud.com>
2023-09-20 01:03:11 +1000

    added in part of the text parsing for input

--- MipsCompiler.cpp
+++ MipsCompiler.cpp
@@ -17,11 +17,12 @@
 #include<map>
 #include<string>
 
-enum class File_type
+enum class Var_type
 {
     INTEGER,
     STRING,
-    DOUBLE
+    DOUBLE,
+    EMPTY
 };
 
 enum class Operation_type
@@ -34,22 +35,27 @@ enum class Operation_type
     SUBTRACT,
     MULTIPLY,
     DIVIDE,
+    MOD,
     EMPTY,
     IF,
     ELSE,
-    RETURN
+    RETURN,
+    FOR,
+    WHILE,
 };
 
 struct Variable_Locate
 {
-    enum File_type type;
+    enum Var_type type;
     int line_number;
+    int char_position;
 };
 
 struct Operation_Locate
 {
     enum Operation_type type;
     int line_number;
+    int char_position;
 };
 
 
@@ -62,6 +68,8 @@ public:
     std::ifstream infile;
     bool open_state;
     int line_count = 0;
+
+    std::vector<std::string> keywords;
 
     std::vector<struct Operation_Locate> Op_Store;
     std::vector<struct Variable_Locate> Var_Store;
@@ -98,6 +106,28 @@ public:
         
         std::cout<<std::endl<<"compiling "<<in_filepath<<" into "<<output<<std::endl;
         open_state = true;
+        
+        //add possible keywords to search for in the parse.
+
+        keywords.push_back("int");
+        keywords.push_back("string(");
+        keywords.push_back("double(");
+
+        keywords.push_back("printf(");
+        keywords.push_back("scanf(");
+
+        keywords.push_back("+");
+        keywords.push_back("-");
+        keywords.push_back("/");
+        keywords.push_back("%");
+
+        keywords.push_back("return");
+        keywords.push_back("while");
+        keywords.push_back("if");
+        keywords.push_back("else");
+        keywords.push_back("{");
+        keywords.push_back("}");
+
     }
 
 //these are all the output functions, i.e adds to the a.out
@@ -170,6 +200,28 @@ class MIPS : public File//this class stores 10 variables, i need to figure out h
                 return 0;
             }
         }
+
+        std::string get_line(int n)
+        {
+            
+            if(n > line_count)
+            {
+                return NULL;
+            }
+            int current = 0;
+            std::string result;
+            while(getline(infile,result))
+            {
+                if(current == n)
+                {
+                    return result;
+                }
+                current++;
+            }
+            
+
+            return result;
+        }
 //all of the input and output stuff
 //i think all the arguments should be of type string cus thats how we named the 'variables'. take from the predefined map we just made. oh but like we can build this into the parser
 // idk though, nah lets just let the parser do the
@@ -203,15 +255,46 @@ class MIPS : public File//this class stores 10 variables, i need to figure out h
         {
             return true;
         }
+        enum Var_type Find_Var(std::string line) //this checks if there contains any variable keywords, if none returns empty.
+        {
+            for (int i = 0 ; i < 3 ; i++)
+            {
+                std::size_t found = line.find(keywords[i]);
+                if ( found != std::string::npos)
+                {
+                    if(line[found + keywords[i].size()] != ' '); //if the character after the word is not a space we discard.
+                    {
+                        continue;
+                    }
+                    switch(i)
+                    {
+                        case 0:
+                            return Var_type::INTEGER;
+                        break;
+                        
+                        case 1:
+                            return Var_type::STRING;
+                        break;
 
+                        case 2:
+                            return Var_type::DOUBLE;
+                        break;
 
-        void parser()
+                    }
+                    
+                }
+            }
+            return Var_type::EMPTY;
+        }
+
+        enum Operation_type Find_Op(std::string line) //checks if any contains any operation keywords (including variables) returns the enum for that else returns empty.
         {
 
         }
 
+        void parser(); //parses text in the input file into a vector of either operations or variable assigning
 
-
+        void coder(); //adds the represented vectors as mips assembly in the output file.
 };
 
 
@@ -241,5 +324,17 @@ int main(int argc, char** argv) //has 2 arguments, one for filepath to cpp file 
         return 1;
     }
 
+    
+}
+
+
+inline void MIPS::parser()//here comes the hard part, searching for "key words like int, double, string printf etc."
+{
+    std::string current_line;
+    for(int i = 0 ; i < line_count ; i++)
+    {
+        current_line = get_line(i);
+        
+    }
     
 }
--- example.cpp
+++ example.cpp
@@ -1,7 +1,7 @@
 hello bro
 suppp
 __TIMESTAMP__test
-test
+oh shit ambatubuss
 __cpp_raw_strings
 __TIMESTAMP__
 static_asserta
